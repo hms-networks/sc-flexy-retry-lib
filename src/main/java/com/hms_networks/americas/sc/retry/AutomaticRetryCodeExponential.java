04777537aad0e598d8ae77e0b36c99f2605b815d
Alex Hawk <alha@hms.se>
2021-02-16 14:24:01 -0500

    Add exponential auto-retry code class

---
+++ src/main/java/com/hms_networks/americas/sc/retry/AutomaticRetryCodeExponential.java
@@ -0,0 +1,42 @@
+package com.hms_networks.americas.sc.retry;
+
+/**
+ * Abstract class which allows for an implemented method of code to be retried up to the maximum
+ * number of retries specified by {@link #getMaxRetries()} using an exponential backoff algorithm to
+ * determine the delay between each retry.
+ *
+ * @author HMS Networks, MU Americas Solution Center
+ * @since 1.0
+ */
+public abstract class AutomaticRetryCodeExponential extends AutomaticRetryCode {
+
+  /** Constant indicating the number of milliseconds in one (1) second. */
+  private static final long MILLIS_PER_SECOND = 1000;
+
+  /**
+   * Method to return the length of delay (milliseconds) before the specified retry number.
+   *
+   * @param retry retry number
+   * @return number of milliseconds delay before specified retry number
+   */
+  protected long getDelayMillisBeforeRetry(int retry) {
+    // Formula: 2ᶜ - 1
+    long delaySecondsBeforeRetry = (long) (Math.pow(2, retry) - 1);
+    long delayMillisBeforeRetry = delaySecondsBeforeRetry * MILLIS_PER_SECOND;
+
+    // Check if above maximum delay, set to maximum if larger
+    if (delayMillisBeforeRetry > getMaxDelayMillisBeforeRetry()) {
+      delayMillisBeforeRetry = getMaxDelayMillisBeforeRetry();
+    }
+
+    return delayMillisBeforeRetry;
+  }
+
+  /**
+   * Abstract method which must be implemented to return the maximum delay (milliseconds) before
+   * retrying.
+   *
+   * @return maximum number of milliseconds delay before retry
+   */
+  protected abstract long getMaxDelayMillisBeforeRetry();
+}
